b6fb3a6e11ca117f10b97d1daae7265cf401d055
RKiddle <RKiddle@users.noreply.github.com>
2019-08-05 21:33:42 +0700

    Update Literally_formatting.py

--- Formatting_Strings/Literally_formatting.py
+++ Formatting_Strings/Literally_formatting.py
@@ -1,1 +1,8 @@
 print (f"Data science is considered {field1} in the {fact1}st century")
+
+
+
+
+
+For an f-string, use the syntax f"string {variable}". After the variable inside the expression, 
+use the type !r to convert to string and show the quotes, and :d for specifying digits.
