c7725feca0cfcee4a7a18edf4b4848d42f90b06d
RKiddle <RKiddle@users.noreply.github.com>
2019-10-27 21:14:19 +0700

    Update Literally_formatting.py

--- Formatting_Strings/Literally_formatting.py
+++ Formatting_Strings/Literally_formatting.py
@@ -1,8 +1,19 @@
-print (f"Data science is considered {field1} in the {fact1}st century")
-
-
-
-
-
+"""
 For an f-string, use the syntax f"string {variable}". After the variable inside the expression, 
 use the type !r to convert to string and show the quotes, and :d for specifying digits.
+"""
+
+# Complete the f-string
+print(f"Data science is considered {field1!r} in the {fact1:d}st century")
+
+""""
+
+For f-string, use the syntax f"string {variable}". 
+After the variable inside the expression use :e for specifying that you want exponential notation.
+
+""""
+# Complete the f-string
+print(f"About {fact2:e} of {field2} in the world")
+
+# Complete the f-string
+print(f"{field3} create around {fact3:.2f}% of the data but only {fact4:.1f}% is analyzed")
